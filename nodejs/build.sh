b69a8c74267bb900b9348692b49443e5ce839e3c
Lam Nguyen <nblam1994@gmail.com>
2021-10-10 20:29:38 -0700

    Update build.sh

--- nodejs/build.sh
+++ nodejs/build.sh
@@ -3,8 +3,8 @@
 echo $NODE_JS_REPO
 echo $NODE_JS_DIR
 echo $DOCKERHUB_USERNAME
-git clone $NODE_JS_REPO
-cd $NODE_JS_DIR
-docker build -t $DOCKERHUB_USERNAME/$NODE_JS_DIR:latest .
-cd ..
-rm -r $NODE_JS_DIR
+# git clone $NODE_JS_REPO
+# cd $NODE_JS_DIR
+# docker build -t $DOCKERHUB_USERNAME/$NODE_JS_DIR:latest .
+# cd ..
+# rm -r $NODE_JS_DIR
