f5701e46e1d979523025e850a0d9c2ea019c81f8
lnguyen-cpi <nblam1994@gmail.com>
2021-10-10 20:16:15 -0700

    fix bad sub

--- nodejs/build.sh
+++ nodejs/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git clone $(NODE_JS_REPO)
-cd $(NODE_JS_DIR)
-docker build -t $(DOCKERHUB_USERNAME)/$(NODE_JS_DIR:latest) .
+git clone $NODE_JS_REPO
+cd $NODE_JS_DIR
+docker build -t $DOCKERHUB_USERNAME/$NODE_JS_DIR:latest .
 cd ..
-rm -r $(NODE_JS_DIR)
+rm -r $NODE_JS_DIR
