9b00dadbe76df285b1369f4b2c28c356609942ff
Lam Nguyen <nblam1994@gmail.com>
2021-10-11 21:24:03 -0700

    Update deploy.sh

--- python/deploy.sh
+++ python/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PWD
-docker stop python-hello
+docker stop $PYTHON_APP
 docker pull $DOCKERHUB_USERNAME/$PYTHON_APP:latest
-docker run --rm -d -p 5000:5000 -e APP_VERSION=latest -e APP_HOST=$HOSTNAME --name=python-hello $DOCKERHUB_USERNAME/$PYTHON_APP:latest
+docker run --rm -d -p 5000:5000 -e APP_VERSION=latest -e APP_HOST=$HOSTNAME --name=$PYTHON_APP $DOCKERHUB_USERNAME/$PYTHON_APP:latest
